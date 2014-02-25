b4bb9dbebd16a6da9d43ef2f5bf4397a5c91268f
Jiri Hnidek <jiri.hnidek@tul.cz>
2014-02-25 12:27:48 +0100

    Moved tess_faces.py to mesh_tools and added some unit tests.

--- io_verse/mesh_tools/tess_faces.py
+++ io_verse/mesh_tools/tess_faces.py
@@ -1,3 +1,5 @@
+#!/bin/usr/env python
+
 # ##### BEGIN GPL LICENSE BLOCK #####
 #
 #  This program is free software; you can redistribute it and/or
@@ -21,32 +23,68 @@
 This module tries to reconstruct tessellated faces
 """
 
+
 def sorted_edge(edge):
     """
     This function returns sorted edge; e.g.: (4, 1) -> (1, 4).
     """
-    return tuple(sorted(edge))
-
-
-def face_edges(face):
-    """
-    Generate list of face edges. TODO: return iterator
-    """
-    face_edges = []
-    prev_vert = None
-    for vert in face:
-        if prev_vert is not None:
-            edge = (prev_vert, vert)
-            face_edges.append(sorted_edge(edge))
-        prev_vert = vert
-    face_edges.append(sorted_edge((face[-1], face[0])))
-    return face_edges
+    return (edge[0], edge[1]) if edge[0] < edge[1] else (edge[1], edge[0])
 
 
 def extend_polygon(polygon, face, inner_edge):
     """
+    This function extend polygon by unique verticies from face that
+    are not part of shared inner edge
     """
     polygon.extend([ vert for vert in face if vert not in inner_edge ])
+
+
+class EdgeLooper(object):
+    """
+    Iterator of Face edges
+    """
+
+    def __init__(self, face):
+        """
+        Constructor of EdgeLooper
+        """
+        self.face = face
+        self.edge_index = 0
+
+    def __iter__(self):
+        """
+        This method returns iterator
+        """
+        return self
+
+    def __getitem__(self, index):
+        """
+        This method returns edge at given index
+        """
+        if index < 0 or index >= len(self.face):
+            raise IndexError
+        else:
+            return (self.face[index], self.face[index + 1]) \
+                if (index + 1) < len(self.face) else \
+                (self.face[index], self.face[0])
+
+    def __next__(self):
+        """
+        This method returns next face of face
+        """
+        try: 
+            edge = self[self.edge_index]
+        except IndexError:
+            raise StopIteration
+        else:
+            self.edge_index += 1
+            return sorted_edge(edge)
+
+    def next(self):
+        """
+        Backward compatibility for Python 2.x
+        """
+        return self.__next__()
 
 
 class Mesh(object):
@@ -65,26 +103,38 @@ class Mesh(object):
 
     def add_verticies(self, verts):
         """
+        This method adds verticies to mesh
         """
         for index, vert in enumerate(verts):
             self.verticies[index] = vert
 
     def add_edges(self, edges):
         """
+        This method adds edges to the mesh
         """
         self.edges.extend([sorted_edge(edge) for edge in edges])
 
     def add_tess_face(self, face):
         """
+        This method adds tessellated face to mesh
         """
+        # Create iterator of all face edges
+        edge_looper = EdgeLooper(face)
         # Create list of inner edges of polygon from face
-        inner_edges = [ edge for edge in face_edges(face) if edge not in self.edges ]
-        # Try to find existing inner edges
+        inner_edges = [ edge for edge in edge_looper if edge not in self.edges ]
+        # Add tesselated face to list of faces
         for inner_edge in inner_edges:
+            # When there is already tesselated face with same inner edge, then
+            # extend this tesselated face with unique verticies from current face
             if inner_edge in self.tess_faces.keys():
                 polygon = self.tess_faces[inner_edge]
                 extend_polygon(polygon, face, inner_edge)
+                # The inner_edge is no longer inner edge, then pop this
+                # key from dictionary
                 self.tess_faces.pop(inner_edge)
+                # When polygon is not included in dictionary of tesselated faces,
+                # then it means, that polygon doesn't contain any inner edges.
+                # The polygon is complete and can be added to the list of polygons.
                 if polygon not in self.tess_faces.values():
                     self.polygons.append(polygon)
             else:
@@ -93,6 +143,7 @@ class Mesh(object):
 
 def main():
     """
+    Main function for testing
     """
     mesh = Mesh()
     mesh.add_verticies(((0, 0, 0), (1, 0, 0), (1, 1, 0), (0, 1, 0)))
@@ -103,6 +154,7 @@ def main():
     print(mesh.edges)
     print(mesh.tess_faces)
     print(mesh.polygons)
+
 
 if __name__ == '__main__':
     main()
